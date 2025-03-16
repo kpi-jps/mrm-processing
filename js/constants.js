715b01014766f9b3feaaf47aa5eb3f8afe80eb52
Joao Pedro da Silva <jp@MacBook-Air-de-Joao.local>
2025-03-16 18:48:24 -0300

    feat: It was created a js file for constants values and a js file to control the index page

---
+++ js/constants.js
@@ -0,0 +1,12 @@
+const constants = Object.freeze({
+    methods: [
+        "output_all_mrm",
+        "filter_by_threshold",
+        "filtered_by_lib"
+    ],
+    hrefs : Object.freeze({
+        "output_all_mrm" : "/html/form1.html",
+        "filter_by_threshold" : "/html/form2.html",
+        "filtered_by_lib" : "/html/form3.html",
+    })
+})
---
+++ js/index.js
@@ -0,0 +1,38 @@
+window.addEventListener("load", (e) => {
+
+    //check if browser can run the application
+    if (!window.sessionStorage) {
+        const msg = "This browser can not run the application!"
+        document.body.innerHTML = `<h3>${msg}</h3>`
+        return
+    }
+    
+    const method = document.getElementById("method")
+    const next = document.getElementById("next")
+
+    //populating the html select element (id = method)
+    for (const m of constants.methods) {
+        const o = document.createElement("option")
+        o.value = m
+        o.innerText = m
+        method.append(o)
+    }
+
+    //setting function to run when html select element (id = method) changes
+    method.addEventListener("change", 
+        //Updates the method value in session storage and the href value for next anchor
+        (e) => {
+        console.log(method.value)
+        sessionStorage.setItem("method", method.value)
+        const href = constants.hrefs[method.value]
+        next.setAttribute("href", href)
+        console.log(href)
+    })
+
+    //saving the initial "method" in session storage
+    sessionStorage.setItem("method", method.value)
+
+    //setting the initial href value for "next" anchor
+    next.setAttribute("href", constants.hrefs[method.value])
+})
+
