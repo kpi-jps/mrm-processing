079040c7fc847fcfb25c2ac6fbf460deca565961
Joao Pedro da Silva <jp@MacBook-Air-de-Joao.local>
2025-03-17 15:40:03 -0300

    refactor: It was changed the constants.js

--- js/constants.js
+++ js/constants.js
@@ -1,12 +1,7 @@
 const constants = Object.freeze({
     methods: {
-        "output_all_mrm" : "output_all_mrm",
-        "filter_by_threshold" : "filter_by_threshold",
-        "filtered_by_lib" : "filtered_by_lib"
-    },
-    hrefs: Object.freeze({
         "output_all_mrm": "/html/form1.html",
         "filter_by_threshold": "/html/form2.html",
-        "filtered_by_lib": "/html/form3.html",
-    })
+        "filtered_by_lib": "/html/form3.html"
+    }
 })
--- js/index.js
+++ js/index.js
@@ -6,7 +6,10 @@ window.addEventListener("load", (e) => {
         document.body.innerHTML = `<h3>${msg}</h3>`
         return
     }
+
     
+
+
     const method = document.getElementById("method")
     const next = document.getElementById("next")
 
@@ -19,20 +22,20 @@ window.addEventListener("load", (e) => {
     }
 
     //setting function to run when html select element (id = method) changes
-    method.addEventListener("change", 
+    method.addEventListener("change",
         //Updates the method value in session storage and the href value for next anchor
         (e) => {
-        console.log(method.value)
-        sessionStorage.setItem("method", method.value)
-        const href = constants.hrefs[method.value]
-        next.setAttribute("href", href)
-        console.log(href)
-    })
+            console.log(method.value)
+            sessionStorage.setItem("method", method.value)
+            const href = constants.methods[method.value]
+            next.setAttribute("href", href)
+            console.log(href)
+        })
 
     //saving the initial "method" in session storage
     sessionStorage.setItem("method", method.value)
 
     //setting the initial href value for "next" anchor
-    next.setAttribute("href", constants.hrefs[method.value])
+    next.setAttribute("href", constants.methods[method.value])
 })
 
